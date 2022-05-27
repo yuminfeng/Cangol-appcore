0b6db17eed22bf1875d02af42abf7cc0e76481d2
yumf <370983992@qq.com>
2022-05-27 10:52:09 +0800

    *): update SessionService

--- appcore/src/main/java/mobi/cangol/mobile/service/session/SessionServiceImpl.java
+++ appcore/src/main/java/mobi/cangol/mobile/service/session/SessionServiceImpl.java
@@ -1,9 +1,16 @@
 package mobi.cangol.mobile.service.session;
 
+import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.Serializable;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import mobi.cangol.mobile.CoreApplication;
@@ -12,6 +19,7 @@ import mobi.cangol.mobile.service.AppService;
 import mobi.cangol.mobile.service.Service;
 import mobi.cangol.mobile.service.ServiceProperty;
 import mobi.cangol.mobile.service.conf.ConfigService;
+import mobi.cangol.mobile.utils.FileUtils;
 
 /**
  * Created by weixuewu on 15/10/24.
@@ -74,6 +82,16 @@ class SessionServiceImpl implements SessionService {
     }
 
     @Override
+    public Session getUserSession(String name) {
+        if (mDebug) Log.d(TAG, "getUserSession " + name);
+        if (mSessionMap.containsKey(name)) {
+            return mSessionMap.get(name);
+        } else {
+            return newSession(mContext, name);
+        }
+    }
+
+    @Override
     public Session getSession(String name) {
         if (mDebug) Log.d(TAG, "getUserSession " + name);
         if (mSessionMap.containsKey(name)) {
@@ -102,13 +120,137 @@ class SessionServiceImpl implements SessionService {
     }
 
     @Override
+    public boolean containsKey(String key) {
+        return mSession.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return mSession.containsValue(value);
+    }
+
+    @Override
+    public int getInt(String key, int defValue) {
+        return mSession.getInt(key, defValue);
+    }
+
+    @Override
+    public boolean getBoolean(String key, boolean defValue) {
+        return mSession.getBoolean(key, defValue);
+    }
+
+    @Override
+    public long getLong(String key, long defValue) {
+        return mSession.getLong(key, defValue);
+    }
+
+    @Override
+    public float getFloat(String key, float defValue) {
+        return mSession.getFloat(key, defValue);
+    }
+
+    @Override
+    public String getString(String key, String defValue) {
+        return mSession.getString(key, defValue);
+    }
+
+    @Override
+    public Set<String> getStringSet(String key, Set<String> defValue) {
+        return mSession.getStringSet(key, defValue);
+    }
+
+    @Override
+    public JSONObject getJSONObject(String key) {
+        return mSession.getJSONObject(key);
+    }
+
+    @Override
+    public JSONArray getJSONArray(String key) {
+        return mSession.getJSONArray(key);
+    }
+
+    @Override
+    public Serializable getSerializable(String key) {
+        return mSession.getSerializable(key);
+    }
+
+    @Override
+    public void saveInt(String key, int value) {
+        mSession.saveInt(key, value);
+    }
+
+    @Override
+    public void saveBoolean(String key, boolean value) {
+        mSession.saveBoolean(key, value);
+    }
+
+    @Override
+    public void saveFloat(String key, float value) {
+        mSession.saveFloat(key, value);
+    }
+
+    @Override
+    public void saveLong(String key, long value) {
+        mSession.saveLong(key, value);
+    }
+
+    @Override
+    public void saveString(String key, String value) {
+        mSession.saveString(key, value);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    public void saveStringSet(String key, Set<String> value) {
+        mSession.saveStringSet(key, value);
+    }
+
+    @Override
+    public void saveJSONObject(String key, JSONObject value) {
+        mSession.saveJSONObject(key, value);
+    }
+
+    @Override
+    public void saveJSONArray(String key, JSONArray value) {
+        mSession.saveJSONArray(key, value);
+    }
+
+    @Override
+    public void saveSerializable(String key, Serializable value) {
+        mSession.saveSerializable(key, value);
+    }
+
+    @Override
+    public void saveAll(Map<String, ?> map) {
+        mSession.saveAll(map);
+    }
+
+    @Override
+    public Object get(String key) {
+        return mSession.get(key);
+    }
+
+    @Override
+    public void put(String key, Object value) {
+        mSession.put(key, value);
+    }
+
+    @Override
+    public void putAll(Map<String, ?> map) {
+        mSession.putAll(map);
+    }
+
+    @Override
+    public void remove(String key) {
+        mSession.remove(key);
+    }
+
+    public void refresh() {
+        mSession.refresh();
+    }
+
+    @Override
     public void clearAll() {
-        for (final Map.Entry<String, Session> entry : mSessionMap.entrySet()) {
-            final Session session = entry.getValue();
-            if (session != null) {
-                session.clearAll();
-            }
-        }
-        mSessionMap.clear();
+        mSession.clearAll();
     }
 }
