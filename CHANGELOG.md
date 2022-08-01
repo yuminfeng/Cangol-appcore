5a01c95711c1eb5e5b67561044d0ba1a400e586d
yumf <370983992@qq.com>
2022-08-01 10:59:10 +0800

    *): remove libdemo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,6 @@
 ## 1.2.8
 - 设置 build:gradle 4.2.2
 
+## 1.2.9  
+- remove libdemo
+- 
--- appcore-demo/src/main/java/mobi/cangol/mobile/appcore/demo/MobileApplication.java
+++ appcore-demo/src/main/java/mobi/cangol/mobile/appcore/demo/MobileApplication.java
@@ -6,8 +6,6 @@ import androidx.appcompat.app.AppCompatDelegate;
 
 import hugo.weaving.DebugLog;
 import mobi.cangol.mobile.CoreApplication;
-import mobi.cangol.mobile.appcore.demo.fragment.TestFragment;
-import mobi.cangol.mobile.appcore.libdemo.LibApplication;
 import mobi.cangol.mobile.service.AppService;
 import mobi.cangol.mobile.service.route.RouteService;
 import mobi.cangol.mobile.stat.StatAgent;
@@ -22,9 +20,8 @@ public class MobileApplication extends CoreApplication {
     public void onCreate() {
         this.setDevMode(true);
         this.setAsyncInit(false);
-        this.getModuleManager().addModule(new LibApplication());
         super.onCreate();
-        this.registerRoute();
+//        this.registerRoute();
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
 
         getSession().remove("");
@@ -36,15 +33,15 @@ public class MobileApplication extends CoreApplication {
             StatAgent.initInstance(this);
         }
     }
-    private void registerRoute() {
-        RouteService routeService=  this.getAppService(AppService.ROUTE_SERVICE);
-        routeService.registerByAnnotation(TestFragment.class);
-    }
+//    private void registerRoute() {
+//        RouteService routeService=  this.getAppService(AppService.ROUTE_SERVICE);
+//        routeService.registerByAnnotation(TestFragment.class);
+//    }
 
     @Override
     public void onExit() {
-        RouteService routeService= this.getAppService(AppService.ROUTE_SERVICE);
-        routeService.unregisterByAnnotation(TestFragment.class);
+//        RouteService routeService= this.getAppService(AppService.ROUTE_SERVICE);
+//        routeService.unregisterByAnnotation(TestFragment.class);
         super.onExit();
     }
 }
